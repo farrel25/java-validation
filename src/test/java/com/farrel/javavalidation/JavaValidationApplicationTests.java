1a4301550cbdddc819175263955056a7d73d2196
Farrel Putra <frl25ap@gmail.com>
2024-04-16 07:52:56 +0700

    Initial commit

---
+++ src/test/java/com/farrel/javavalidation/JavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farrel.javavalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
